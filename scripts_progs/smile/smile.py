8cd309108e63d2d2554f12af7d9b06e770438def
sedihglow <jamesmross4@gmail.com>
2023-06-27 19:04:25 -0700

    python things pretty much

--- scripts_progs/smile/smile.py
+++ scripts_progs/smile/smile.py
@@ -2,23 +2,65 @@
 
 import time
 import random
+import sys
+import signal
+import os
 
-faces = [":)", "^_^", "-_-", ":P", ";)", "-_-;", "^_^;", "(>^_^)>", "<(^_^<)"]
+
 bye_falicia = "): | D: | ); | D; | :$ |"
+def sigint_handler(signum, handler):
+    print("\n\n Camera and Recording has initiated. HI\n\n")
+    sys.exit(0)
 
-random.seed()
+def print_sigs():
+    valid = signal.valid_signals()
 
-while (True):
+    print("Valid Signals:")
+    for sig in valid:
+        cell_type = type(sig)
+
+        #if (cell_type ==
+        """
+        print(f"-- valid_signals() list element values and type --\n"
+              f"data element type: {cell_type}\n"
+              f"sig_name: {}\n"
+              f"sig_num : {}")
+        """
+
+def print_sig_sigs_enum():
+    print("Members of signal.Signals:")
+    for member in signal.Signals:
+        print(f"{member.name} = {member.value}")
+
+def print_sigs_meta_enum():
+    meta = signal.Signals.__class__
+
+def print_valid_sigs(pretty: bool) -> None:
+    print(type(signal.valid_signals()))
+
+if __name__ == "__main__":
+    faces = [":)", "^_^", "-_-", ":P", ";)", "-_-;", "^_^;", "(>^_^)>", "<(^_^<)"]
+
+    print("\n\n------------\n\n")
+    print_sigs()
+
+    #print_sig_sigs_enum()
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    """
+
+    random.seed()
+
     try:
-        random.shuffle(faces)
+        while (True):
+            random.shuffle(faces)
 
-        for face in faces:
-            print(face, end=' | ')
+            for face in faces:
+                print(face, end=' | ')
+            print()
 
-        print()
-
-        time.sleep(1)
+            time.sleep(1)
     except KeyboardInterrupt:
         print("\n\n", bye_falicia)
-        break
 
+    """

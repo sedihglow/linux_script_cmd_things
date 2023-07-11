d0a88d740273b553a94574ab233c0fcfcc596914
sedihglow <jamesmross4@gmail.com>
2023-07-11 04:45:10 -0700

    making it all up to the date

--- scripts_progs/smile/smile.py
+++ scripts_progs/smile/smile.py
@@ -6,48 +6,15 @@ import sys
 import signal
 import os
 
-
 bye_falicia = "): | D: | ); | D; | :$ |"
 def sigint_handler(signum, handler):
     print("\n\n Camera and Recording has initiated. HI\n\n")
     sys.exit(0)
 
-def print_sigs():
-    valid = signal.valid_signals()
-
-    print("Valid Signals:")
-    for sig in valid:
-        cell_type = type(sig)
-
-        #if (cell_type ==
-        """
-        print(f"-- valid_signals() list element values and type --\n"
-              f"data element type: {cell_type}\n"
-              f"sig_name: {}\n"
-              f"sig_num : {}")
-        """
-
-def print_sig_sigs_enum():
-    print("Members of signal.Signals:")
-    for member in signal.Signals:
-        print(f"{member.name} = {member.value}")
-
-def print_sigs_meta_enum():
-    meta = signal.Signals.__class__
-
-def print_valid_sigs(pretty: bool) -> None:
-    print(type(signal.valid_signals()))
-
 if __name__ == "__main__":
     faces = [":)", "^_^", "-_-", ":P", ";)", "-_-;", "^_^;", "(>^_^)>", "<(^_^<)"]
 
-    print("\n\n------------\n\n")
-    print_sigs()
-
-    #print_sig_sigs_enum()
-    signal.signal(signal.SIGINT, sigint_handler)
-
-    """
+    #signal.signal(signal.SIGINT, sigint_handler)
 
     random.seed()
 
@@ -62,5 +29,3 @@ if __name__ == "__main__":
             time.sleep(1)
     except KeyboardInterrupt:
         print("\n\n", bye_falicia)
-
-    """
